3daf950df796a0e405d3ad9f73226106b3fca8d7
Marcus Jun <mjun@calstate.edu>
2018-04-12 22:14:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
 # code here!
+
+class School
+  attr_accessor :school, :roster
+
+  def initialize (school)
+    @school=school
+    @roster={}
+  end
+
+  def add_student(student,grade)
+    #@roster[grade]=[]
+    if @roster.keys.none? {|num| num==grade}
+      @roster[grade]=[student]
+    else
+      @roster[grade]<<student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]#.values.flatten
+    #student_array=[]
+    #student_array=@roster.collect do |num,student|
+      #if num===grade
+        #student_array<<student
+        #student
+      #end
+    #end
+    #student_array
+  end
+
+  def sort
+    @roster.each do |num,name|
+      #num=num.sort {|a,b| a<=>b}
+      #name=name.sort {|a,b| a<=>b}
+      name.sort!
+    end
+  end
+
+
+end
